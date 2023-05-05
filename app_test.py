63ee9e2888a47cc2eacae5b4172dca5dc8b2c1a4
Addwin Fernando <addwinf07@gmail.com>
2023-05-05 21:19:40 +0530

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -11,12 +11,12 @@ def test_app():
 
     # Test if the API returns "Unlocked" for valid data
     response = client.post(
-        '/receive_data', data={"center-lat": 80.09734748801102, "center-lon": 12.919357753640268, "lat": 80.09738668239515, "lon": 12.919405460455502})
+        '/receive_data', json={"center-lat": 80.09734748801102, "center-lon": 12.919357753640268, "lat": 80.09738668239515, "lon": 12.919405460455502})
     assert response.status_code == 200
     assert response.get_data().decode() == "Unlocked"
 
     # Test if the API returns "Locked" for invalid data
     response = client.post(
-        '/receive_data', data={"center-lat": 80.09734748801102, "center-lon": 12.919357753640268, "lat": 80.09738668239515, "lon": 13.919405460455502})
+        '/receive_data', json={"center-lat": 80.09734748801102, "center-lon": 12.919357753640268, "lat": 80.09738668239515, "lon": 13.919405460455502})
     assert response.status_code == 200
     assert response.get_data().decode() == "Locked"
